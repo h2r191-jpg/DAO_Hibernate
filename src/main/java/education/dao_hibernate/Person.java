022de1609c192663b60b662877301623ef61093e
h2r <h2r191@gmail.com>
2026-05-03 17:43:53 +0700

    Убрана @Data.

--- src/main/java/education/dao_hibernate/Person.java
+++ src/main/java/education/dao_hibernate/Person.java
@@ -37,8 +37,13 @@ public class Person {
     public final boolean equals(Object o) {
         if (this == o) return true;
         if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o)
+                .getHibernateLazyInitializer()
+                .getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? (
+                (HibernateProxy) this)
+                .getHibernateLazyInitializer()
+                .getPersistentClass() : this.getClass();
         if (thisEffectiveClass != oEffectiveClass) return false;
         Person person = (Person) o;
         return getId() != null && Objects.equals(getId(), person.getId());
@@ -46,7 +51,11 @@ public class Person {
 
     @Override
     public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+        return this instanceof HibernateProxy ? ((HibernateProxy) this)
+                .getHibernateLazyInitializer()
+                .getPersistentClass()
+                .hashCode() : getClass()
+                .hashCode();
     }
 
     @Override
