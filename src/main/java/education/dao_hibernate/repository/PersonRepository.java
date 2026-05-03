70bd3616223e8f231f524cb03358174934296696
h2r <h2r191@gmail.com>
2026-05-03 12:23:28 +0700

    Миграции c Hibernate, Liquibase

--- src/main/java/education/dao_hibernate/repository/PersonRepository.java
+++ src/main/java/education/dao_hibernate/repository/PersonRepository.java
@@ -4,9 +4,7 @@ import education.dao_hibernate.Person;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 public class PersonRepository {
@@ -15,14 +13,9 @@ public class PersonRepository {
     private EntityManager entityManager;
 
     public List<Person> getPersonsByCity(String city) {
-        List<Person> allPersons = entityManager.createQuery(
-                "SELECT p FROM Person p",
-                Person.class
-        ).getResultList();
-
-        return allPersons.stream()
-                .filter(person -> city.equalsIgnoreCase(
-                        person.getCityOfLiving()))
-                .collect(Collectors.toList());
+        return entityManager.createQuery(
+                        "SELECT p FROM Person p WHERE p.cityOfLiving = :city", Person.class)
+                .setParameter("city", city)
+                .getResultList();
     }
 }
